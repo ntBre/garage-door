2ceae3500eb31b58e27b671f0ab6eab83986596e
Brent Westbrook <brentrwestbrook@gmail.com>
2023-08-14 21:19:36 -0400

    move CollectionGetBody to module, store client on FractalClient
    this way each *GetBody can have its own Data and QueryFilter definitions

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@
 
 use std::collections::HashMap;
 
+use reqwest::Client;
 use serde::Serialize;
+
+use crate::collection::CollectionGetBody;
 
 const ADDR: &str = "https://api.qcarchive.molssi.org:443/";
 
@@ -10,6 +13,7 @@ struct FractalClient {
     address: &'static str,
     headers: HashMap<String, String>,
     encoding: &'static str,
+    client: Client,
 }
 
 impl FractalClient {
@@ -18,6 +22,7 @@ impl FractalClient {
             address: ADDR,
             headers: HashMap::new(),
             encoding: "msgpack-ext",
+            client: Client::new(),
         };
         ret.headers.insert(
             "Content-Type".to_owned(),
@@ -27,49 +32,70 @@ impl FractalClient {
             .insert("User-Agent".to_owned(), "qcportal/0.15.7".to_owned());
         ret
     }
+
+    async fn get(&self, collection: CollectionGetBody) -> reqwest::Response {
+        let url = format!("{}collection", self.address);
+        self.client
+            .get(url)
+            .body(collection.to_json().unwrap())
+            .header("Content-Type", "application/json")
+            .send()
+            .await
+            .unwrap()
+    }
 }
 
-#[derive(Serialize)]
-struct QueryFilter {
-    include: Option<bool>,
-    exclude: Option<bool>,
-}
+mod collection {
+    use serde::Serialize;
 
-#[derive(Serialize)]
-struct Data {
-    collection: String,
-    name: String,
-}
+    #[derive(Serialize)]
+    struct QueryFilter {
+        include: Option<bool>,
+        exclude: Option<bool>,
+    }
 
-#[derive(Serialize)]
-struct CollectionGetBody {
-    meta: QueryFilter,
-    data: Data,
+    #[derive(Serialize)]
+    struct Data {
+        collection: String,
+        name: String,
+    }
+
+    #[derive(Serialize)]
+    pub struct CollectionGetBody {
+        meta: QueryFilter,
+        data: Data,
+    }
+
+    impl CollectionGetBody {
+        pub fn new(
+            collection: impl Into<String>,
+            name: impl Into<String>,
+        ) -> Self {
+            Self {
+                meta: QueryFilter {
+                    include: None,
+                    exclude: None,
+                },
+                data: Data {
+                    collection: collection.into(),
+                    name: name.into(),
+                },
+            }
+        }
+
+        pub fn to_json(&self) -> Result<String, serde_json::Error> {
+            serde_json::to_string(&self)
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    let collection = CollectionGetBody {
-        meta: QueryFilter {
-            include: None,
-            exclude: None,
-        },
-        data: Data {
-            collection: "torsiondrivedataset".to_owned(),
-            name: "OpenFF multiplicity correction torsion drive data v1.1"
-                .to_owned(),
-        },
-    };
-    let buf = serde_json::to_string(&collection).unwrap();
-    let frac = FractalClient::new();
-    let url = format!("{}collection", frac.address);
-    let client = reqwest::Client::new();
-    let response = client
-        .get(url)
-        .body(buf)
-        .header("Content-Type", "application/json")
-        .send()
-        .await
-        .unwrap();
+    let collection = CollectionGetBody::new(
+        "torsiondrivedataset",
+        "OpenFF multiplicity correction torsion drive data v1.1",
+    );
+    let client = FractalClient::new();
+    let response = client.get(collection).await;
     dbg!(response);
 }
