3b7a0ba766d2d86d69548cae26db7cd8e3daf8c1
Brent Westbrook <brentrwestbrook@gmail.com>
2023-08-16 18:22:28 -0400

    fmt req.py

--- python/req.py
+++ python/req.py
@@ -1,7 +1,11 @@
 import requests
 
-data = b'\x82\xa4meta\x82\xa7include\xc0\xa7exclude\xc0\xa4data\x82\xaacollection\xb3torsiondrivedataset\xa4name\xd96OpenFF multiplicity correction torsion drive data v1.1'
-r = requests.get("https://api.qcarchive.molssi.org:443/collection", data=data, headers={'Content-Type': 'application/msgpack-ext'})
+data = b"\x82\xa4meta\x82\xa7include\xc0\xa7exclude\xc0\xa4data\x82\xaacollection\xb3torsiondrivedataset\xa4name\xd96OpenFF multiplicity correction torsion drive data v1.1"
+r = requests.get(
+    "https://api.qcarchive.molssi.org:443/collection",
+    data=data,
+    headers={"Content-Type": "application/msgpack-ext"},
+)
 print(r.request.url)
 print(r.request.headers)
 print(r.request.body)
