a61d5ce38d33250a6d34e7bc9f68e2efab9fc01c
Brent Westbrook <brentrwestbrook@gmail.com>
2023-08-16 19:22:57 -0400

    move client to its own module and file

---
+++ src/client.rs
@@ -0,0 +1,65 @@
+use reqwest::{header::HeaderMap, Client};
+
+use crate::{collection::CollectionGetBody, procedure::ProcedureGetBody};
+
+pub struct FractalClient {
+    address: &'static str,
+    headers: HeaderMap,
+    client: Client,
+}
+
+impl FractalClient {
+    pub fn new() -> Self {
+        const ADDR: &str = "https://api.qcarchive.molssi.org:443/";
+        let mut ret = Self {
+            address: ADDR,
+            headers: HeaderMap::new(),
+            client: Client::new(),
+        };
+        ret.headers
+            .insert("Content-Type", "application/json".parse().unwrap());
+        ret.headers
+            .insert("User-Agent", "qcportal/0.15.7".parse().unwrap());
+        ret
+    }
+
+    pub async fn get_information(&self) {
+        let url = format!("{}information", self.address);
+        let response = self
+            .client
+            .get(url)
+            .headers(self.headers.clone())
+            .send()
+            .await
+            .unwrap();
+        assert_eq!(response.status(), 200);
+    }
+
+    pub async fn get_collection(
+        &self,
+        collection: CollectionGetBody,
+    ) -> reqwest::Response {
+        let url = format!("{}collection", self.address);
+        self.client
+            .get(url)
+            .body(collection.to_json().unwrap())
+            .headers(self.headers.clone())
+            .send()
+            .await
+            .unwrap()
+    }
+
+    pub async fn get_procedure(
+        &self,
+        collection: ProcedureGetBody,
+    ) -> reqwest::Response {
+        let url = format!("{}procedure", self.address);
+        self.client
+            .get(url)
+            .body(collection.to_json().unwrap())
+            .headers(self.headers.clone())
+            .send()
+            .await
+            .unwrap()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use client::FractalClient;
 use procedure::ProcedureGetBody;
-use reqwest::{header::HeaderMap, Client};
 
 use collection::CollectionGetBody;
 
@@ -7,73 +7,12 @@ use crate::{
     collection::CollectionGetResponse, procedure::ProcedureGetResponse,
 };
 
+mod client;
 mod collection;
 mod procedure;
 
 #[cfg(test)]
 mod tests;
-
-struct FractalClient {
-    address: &'static str,
-    headers: HeaderMap,
-    client: Client,
-}
-
-impl FractalClient {
-    fn new() -> Self {
-        const ADDR: &str = "https://api.qcarchive.molssi.org:443/";
-        let mut ret = Self {
-            address: ADDR,
-            headers: HeaderMap::new(),
-            client: Client::new(),
-        };
-        ret.headers
-            .insert("Content-Type", "application/json".parse().unwrap());
-        ret.headers
-            .insert("User-Agent", "qcportal/0.15.7".parse().unwrap());
-        ret
-    }
-
-    async fn get_information(&self) {
-        let url = format!("{}information", self.address);
-        let response = self
-            .client
-            .get(url)
-            .headers(self.headers.clone())
-            .send()
-            .await
-            .unwrap();
-        assert_eq!(response.status(), 200);
-    }
-
-    async fn get_collection(
-        &self,
-        collection: CollectionGetBody,
-    ) -> reqwest::Response {
-        let url = format!("{}collection", self.address);
-        self.client
-            .get(url)
-            .body(collection.to_json().unwrap())
-            .headers(self.headers.clone())
-            .send()
-            .await
-            .unwrap()
-    }
-
-    async fn get_procedure(
-        &self,
-        collection: ProcedureGetBody,
-    ) -> reqwest::Response {
-        let url = format!("{}procedure", self.address);
-        self.client
-            .get(url)
-            .body(collection.to_json().unwrap())
-            .headers(self.headers.clone())
-            .send()
-            .await
-            .unwrap()
-    }
-}
 
 #[tokio::main]
 async fn main() {
