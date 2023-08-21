c4d0506e55a6b935f1d8b9dafeef124e77e99d86
Brent Westbrook <brentrwestbrook@gmail.com>
2023-08-21 17:37:37 -0400

    detect record type automatically

--- python/shim.py
+++ python/shim.py
@@ -3,22 +3,37 @@
 
 import json
 import logging
+import sys
 
 import numpy as np
 from openff.toolkit import Molecule
 from openff.units import unit
-from qcportal.models import TorsionDriveRecord
+from qcportal.models import OptimizationRecord, TorsionDriveRecord
 
 logging.getLogger("openff.toolkit").setLevel(logging.ERROR)
 
-with open("/tmp/td.json", "r") as infile:
+arg = sys.argv[1]
+
+with open(arg, "r") as infile:
     results = json.load(infile)
 
+
+def get_type(results):
+    typ = results[0][0]["procedure"]
+    match typ:
+        case "optimization":
+            return OptimizationRecord
+        case "torsiondrive":
+            return TorsionDriveRecord
+    raise ValueError(f"Unrecognized record type: {typ}")
+
+
+typ = get_type(results)
 for r in results:
     [record, cmiles, conformers] = r
     molecule = Molecule.from_mapped_smiles(cmiles, allow_undefined_stereo=True)
     molecule.add_conformer(
         np.array(conformers[0], float).reshape(-1, 3) * unit.bohr
     )
-    record = TorsionDriveRecord.parse_obj(record)
+    record = typ.parse_obj(record)
     print(record, molecule)
